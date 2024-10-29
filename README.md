71a052e309c63945c49de50c0a05eb45b0ff203f
Pablo Matías Monsalvo <polargentino@gmail.com>
2024-10-29 19:03:38 -0300

    Desde los laboratorios del MIT

---
+++ README.md
@@ -0,0 +1,1 @@
+# fotovoltaica por pol
---
+++ script.js
@@ -0,0 +1,9 @@
+// Añadir efectos de scroll suave para la navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
